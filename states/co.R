55547c0e455948049d395176b9900ef01645ae47
Pete Deevakul <editor@mozzarella.website>
2016-05-21 10:44:31 -0400

    Created EDA-vis directory, added CO plots
    directory for visualizations and Colorado state plots

--- states/co.R
+++ states/co.R
@@ -23,19 +23,21 @@ proj4string(natural) <- co.CRS
 proj4string(water) <- co.CRS
 summary(admin)
 
-# steelblue4 background
-par(mar = c(0, 0, 0, 0), bg = "#4682B4")
+library(mapproj)
+co.proj <- mapproject(co$lon, co$lat, projection = "mercator")
+
+par(mar = c(0, 0, 0, 0), bg = "grey23")
 plot(0, 0, asp = 1, type = "n", bty = "n",
      xlim = c(-109.0603, -102.0409),
      ylim = c(36.9924, 41.0034),
      xlab = "", ylab = "")
 
-lines(admin, lty = 1, lwd = 0.330, col = "#B8B8B8") # grey82
-lines(natural, lty = 1, lwd = 1.0, col = "#A0522D") # sienna
-lines(water, lty = 1, lwd = 0.600, col = "#00688B") # deepskyblue4
+lines(admin, lty = 1, lwd = 0.330, col = "#B8B8B830") # grey82
+lines(natural, lty = 1, lwd = 1.0, col = "peachpuff") # sienna
+lines(water, lty = 1, lwd = 1.0, col = "#00688B") # deepskyblue4
 
-points(co$lon, co$lat, pch = 16, col = "#CD262640", cex = 2.4) # firebrick3
-points(co$lon, co$lat, pch = 1, col = "#3D3D3D40", cex = 2.4) # grey24
+points(co$lon, co$lat, pch = 16, col = "#CD262640", cex = 2.8) # firebrick3
+points(co$lon, co$lat, pch = 1, col = "#00000035", cex = 2.8) # grey24
 
 # Voronoi and Delaunay
 library(deldir)
@@ -43,7 +45,44 @@ coTess <- deldir(co$lon, co$lat)
 # white lines for del
 
 plot(coTess, wlines = "triang", wpoints = "none", number = FALSE, add = TRUE,
-     lty = 1, lwd = 1.2, col = "#FFFFFF75")
+     lty = 1, lwd = 0.6, col = "#FFFFFF")
 plot(coTess, wlines = "tess", wpoints = "none", number = FALSE, add = TRUE,
-     lty = 2, lwd = 0.6, col = "#FFFFE0")
+     lty = 2, lwd = 0.8, col = "burlywood")
+
+# metropolitan cluster --------------------------------------------------------
+# -105.109927, -104.600296, 39.614431, 39.914247
+
+par(mar = c(0, 0, 0, 0), bg = "grey90")
+plot(0, 0, asp = 1, type = "n", bty = "n",
+     xlim = c(-105.109927, -104.600296),
+     ylim = c(39.614431, 39.914247),
+     xlab = "", ylab = "")
+
+lines(admin, lty = 1, lwd = 0.330, col = "#B8B8B8") # grey82
+lines(natural, lty = 1, lwd = 1.0, col = "peachpuff4") # sienna
+lines(water, lty = 1, lwd = 1.0, col = "#00688B") # deepskyblue4
+
+points(co$lon, co$lat, pch = 16, col = "#CD262680", cex = 2.8) # firebrick3
+points(co$lon, co$lat, pch = 1, col = "#00000070", cex = 2.8) # grey24
+
+# -105.762634, -104.092712, 39.294454, 40.254901 ------------------------------
+par(mar = c(0, 0, 0, 0), bg = "grey90")
+plot(0, 0, asp = 1, type = "n", bty = "n",
+     xlim = c(-105.762634, -104.092712),
+     ylim = c(39.294454, 40.204901),
+     xlab = "", ylab = "")
+
+lines(admin, lty = 1, lwd = 0.330, col = "#B8B8B8") # grey82
+lines(natural, lty = 1, lwd = 1.0, col = "peachpuff4") # sienna
+lines(water, lty = 1, lwd = 1.0, col = "#00688B") # deepskyblue4
+
+points(co$lon, co$lat, pch = 16, col = "#CD262680", cex = 2.8) # firebrick3
+points(co$lon, co$lat, pch = 1, col = "#00000070", cex = 2.8) # grey24
+
+plot(coTess, wlines = "triang", wpoints = "none", number = FALSE, add = TRUE,
+     lty = 1, lwd = 0.6, col = "#FFFFFF")
+plot(coTess, wlines = "tess", wpoints = "none", number = FALSE, add = TRUE,
+     lty = 2, lwd = 0.8, col = "burlywood")
+
+
 
